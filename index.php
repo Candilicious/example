622800bd2aac659669310290a8159205aee9057d
Candilicious <aylav98@mail.ru>
2023-01-07 06:33:45 +0400

    Пример кода

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', '1');*/
 require __DIR__ . "/vendor/autoload.php";
-require_once 'controllers/Controller.php'; //родитель для всех контроллеров
-require_once 'models/Model.php'; //родитель для всех моделей
-require "controllers/FormController.php"; //работа с формой
+require_once 'controllers/Controller.php'; 
+require_once 'models/Model.php'; 
+require "controllers/FormController.php"; 
 
 /*Инициализация phpdotenv*/
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
